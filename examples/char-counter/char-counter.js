48454ef780ca238a0dc49ede064630206473250e
Dave Lesage <dlesage27@gmail.com>
2015-06-27 01:20:52 -0700

    char counter example overhaul to use the new transform callback style to expand values into multiple puts for more succinct code

--- examples/char-counter/char-counter.js
+++ examples/char-counter/char-counter.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import Channel, {timeout} from '../../dist/channel.js';
 let log = console.log.bind(console);
 
-import originalText from './text.js';
+import text from './text.js';
 
 function isAlphanumeric(char) {
     if (!(char instanceof Number))
@@ -17,66 +17,48 @@ function isAlphanumeric(char) {
 }
 
 export async function run() {
-    let chText      = new Channel();
-    let chLines     = new Channel();
-    let chSentences = new Channel();
-    let chWords     = new Channel();
-    let chChars     = new Channel();
-    let charset     = {};
+    try {
+        let start = new Date().getTime();
+        let lines = new Channel((text, accept) => {
+            for (let line of text.split('\n'))
+                accept(line);
+        });
+        let sentences = new Channel((line, accept) => {
+            for (let sentence of line.split('.'))
+                accept(sentence);
+        });
+        let words = new Channel((sentence, accept) => {
+            for (let word of sentence.split(' '))
+                accept(word);
+        });
+        let chars = new Channel((word, accept) => {
+            for (let char of word.split('').filter(isAlphanumeric).map(x => x.toLowerCase()))
+                accept(char);
+        });
 
-    chText.consume(async text => {
-        for (let line of text.split('\n'))
-            await chLines.put(line);
-    });
+        let charset = {};
+        chars.consume(char => {
+            if (!charset[char])
+                charset[char] = 0;
+            charset[char]++;
+        });
 
-    chLines.consume(async line => {
-        for (let sentence of line.split('.'))
-            await chSentences.put(sentence);
-    });
-
-    // split sentences into words, pass to chWords
-    // whenever a sentence is placed on chSentences
-    chSentences.consume(async sentence => {
-        for (let word of sentence.split(' '))
-            await chWords.put(word);
-    });
-
-    // split words into chars, pass to chChars
-    // whenever a word is placed on chWords
-    chWords.consume(async word => {
-        let chars = word
-            .split('')
-            .filter(isAlphanumeric)
-            .map(x => x.toLowerCase());
-        for (let char of chars)
-            await chChars.put(char);
-    });
-
-    // take chars and add them to a running counter
-    // whenever a char is placed chChars
-    chChars.consume(async char => {
-        if (!charset[char])
-            charset[char] = 0;
-        charset[char]++;
-    });
-
-    // kick it all off!
-    // to be explicit, manually wait for dones,
-    // then close the next channel in line
-    await chText.put(originalText);
-    chText.close();
-    await chText.done();
-    chLines.close();
-    await chLines.done();
-    chSentences.close();
-    await chSentences.done();
-    chWords.close();
-    await chWords.done();
-    chSentences.close();
-    await chSentences.done();
-    let chars = Object.entries(charset);
-    chars.sort((a, b) => {
-        return a[1] > b[1] ? -1 : a[1] < b[1] ? 1 : 0;
-    });
-    log(chars);
+        lines.pipe(sentences)
+            .pipe(words)
+            .pipe(chars);
+        log('Parsing text from ./text.js and counting alphanumeric characters...');
+        await lines.put(text);
+        lines.close(true);
+        await chars.done();
+        let sorted = Object.entries(charset).sort((a, b) => {
+            return a[1] > b[1] ? -1 : a[1] < b[1] ? 1 : 0;
+        });
+        log('Character counts were:');
+        log(sorted);
+        let end = new Date().getTime();
+        log(`Counting characters took ${end - start}ms`);
+    }
+    catch(e) {
+        log('error!', e);
+    }
 }
